346d26b327d63ad051ed118990891179f7e63723
Michał Laskowski <m@majkel.cc>
2014-09-12 15:51:53 +0200

    pomodoro breeze tasks list

--- lib/pomodoro/task_list/breeze.rb
+++ lib/pomodoro/task_list/breeze.rb
@@ -1,7 +1,8 @@
 class Pomodoro::TaskList::Breeze < Pomodoro::TaskList::Base
 
-  def initialize user, password
-    @user, @password = user, password
+  def initialize email, password
+    @email = email
+    @client = RestClient::Resource.new("http://api.breeze.pm", :user => @email, :password => password)
   end
 
   def tasks
@@ -12,16 +13,28 @@ class Pomodoro::TaskList::Breeze < Pomodoro::TaskList::Base
     tasks.select { |task| dependent?(task) }
   end
 
-  def fetch_tasks
-  end
-
-  def dependent?(task)
-  end
-
   private
 
-    def get resource
-      RestClient.get("http://#{@user}:#{@password}@api.breeze.pm/" + resource + ".json")
+    def get(resource)
+      JSON.parse @client[resource + ".json"].get
+    end
+
+    def fetch_tasks
+      tasks = []
+      get("projects").each do |project|
+        tasks += get("projects/#{project['id']}/cards")[0]["cards"]
+      end
+      filter_user_tasks(tasks)
+    end
+
+    def dependent?(task)
+      task["users"].count > 1
+    end
+
+    def filter_user_tasks(tasks)
+      tasks.select do |task|
+        task["users"].find { |user| user["email"] == @email }
+      end
     end
 
 end
